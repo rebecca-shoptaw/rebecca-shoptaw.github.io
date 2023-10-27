4bd952ffb561e4205b1ad599cbb56ac32f54d634
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2023-10-27 15:24:30 -0400

    Add spinner.

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { ClipLoader } from "react-spinners";
+
 const site_link = "https://rebecca-shoptaw.github.io";
 const git_link = "https://github.com/rebecca-shoptaw";
 
@@ -144,196 +146,203 @@ const Homepage = () => {
 
   return (
     <>
-      <nav id="navbar" className={`${!loaded ? "hidden" : ""}`}>
-        <div className="nav-content">
-          <span>
-            <a href="#welcome-section">
-              <h1>Rebecca Shoptaw</h1>
-            </a>
-          </span>
-          <span>
-            <div className="nav-links">
-              <a className="nav-link" href="#about-ref">
-                About
-              </a>
-              <a className="nav-link" href="#projects-ref">
-                Work
-              </a>
-              <a id="contact-link" href="#contact-ref">
-                Contact
-              </a>
-            </div>
-          </span>
+      {!loaded && (
+        <div id="loading">
+          <ClipLoader size={100} color="#fff" />
         </div>
-      </nav>
-      <section id="welcome-section" className={`${!loaded ? "hidden" : ""}`}>
-        <div className="title">
-          <h1>Rebecca Shoptaw</h1>
-          <h2>
-            <i>Frontend Developer</i>
-          </h2>
-        </div>
-      </section>
-      <div id="about-ref" className="locator"></div>
-      <section id="about">
-        <div className="section-body">
-          <h1 className="section-title">About</h1>
-          <div className="bio">
-            <span id="bio-block">
-              <p className="bio-text">
-                {`Hello! My name is Rebecca and I'm a
+      )}
+      <div id="site-body" className={`${!loaded ? "hidden" : ""}`}>
+        <nav id="navbar" className={`${!loaded ? "hidden" : ""}`}>
+          <div className="nav-content">
+            <span>
+              <a href="#welcome-section">
+                <h1>Rebecca Shoptaw</h1>
+              </a>
+            </span>
+            <span>
+              <div className="nav-links">
+                <a className="nav-link" href="#about-ref">
+                  About
+                </a>
+                <a className="nav-link" href="#projects-ref">
+                  Work
+                </a>
+                <a id="contact-link" href="#contact-ref">
+                  Contact
+                </a>
+              </div>
+            </span>
+          </div>
+        </nav>
+        <section id="welcome-section" className={`${!loaded ? "hidden" : ""}`}>
+          <div className="title">
+            <h1>Rebecca Shoptaw</h1>
+            <h2>
+              <i>Frontend Developer</i>
+            </h2>
+          </div>
+        </section>
+        <div id="about-ref" className="locator"></div>
+        <section id="about">
+          <div className="section-body">
+            <h1 className="section-title">About</h1>
+            <div className="bio">
+              <span id="bio-block">
+                <p className="bio-text">
+                  {`Hello! My name is Rebecca and I'm a
                 filmmaker-turned-front-end-developer.`}
-                <br></br>
-                <br></br>
-                {`I bring from my background in filmmaking and photo/video editing the combination
+                  <br></br>
+                  <br></br>
+                  {`I bring from my background in filmmaking and photo/video editing the combination
                 of a strong visual sense and minute technical precision.`}
-                <br></br>
-                <br></br>
-                {`My training in coding began in high school on my TI-84 calculator,
+                  <br></br>
+                  <br></br>
+                  {`My training in coding began in high school on my TI-84 calculator,
                 and continued through CS50 in college, for which I learned a
                 number of coding languages and taught myself Swift to build a
                 video-editing app.`}
-                <br></br>
-                <br></br>
-                {`Now, I'm focusing on front-end
+                  <br></br>
+                  <br></br>
+                  {`Now, I'm focusing on front-end
                 development, and using React with a mix of HTML, CSS, and Javascript/Typescript to make beautiful and functional responsive sites and applications.`}
-                <br></br>
-                <br></br>
-              </p>
-              <a href="#contact-ref">Get in Touch</a>
-            </span>
-            <img
-              id="bio-img"
-              src="https://images.squarespace-cdn.com/content/v1/58c9909ce58c627a188f8a64/ad9e7ee7-964e-4151-96a6-e99d81353fd2/20201007.JPG?format=2500w"
-              alt="Profile picture"
-            />
+                  <br></br>
+                  <br></br>
+                </p>
+                <a href="#contact-ref">Get in Touch</a>
+              </span>
+              <img
+                id="bio-img"
+                src="https://images.squarespace-cdn.com/content/v1/58c9909ce58c627a188f8a64/ad9e7ee7-964e-4151-96a6-e99d81353fd2/20201007.JPG?format=2500w"
+                alt="Profile picture"
+              />
+            </div>
           </div>
-        </div>
-      </section>
-      <div id="projects-ref" className="locator"></div>
-      <section id="projects">
-        <div className="section-body">
-          <h1 className="section-title">Work Samples</h1>
-          <div className="project-links">
-            {projects.map((project) => (
-              <div className="project-tile" id={project.id}>
-                <div className="img-wrapper">
-                  <a href={site_link + project.link} target="_blank">
-                    <img
-                      id={project.img_id}
-                      src={project.img_src as string}
-                      alt={project.img_alt}
-                    />
-                  </a>
-                </div>
-                <div className="description">
-                  <p>
-                    <a
-                      href={site_link + project.link}
-                      target="_blank"
-                      className="project-link"
-                    >
-                      {project.title}
-                    </a>
-                    {project.wip ? (
-                      <div>
-                        <i className="wip">Work in Progress</i>
-                      </div>
-                    ) : (
-                      <br></br>
-                    )}
-                    {project.description}
-                    <i className="project-italics">
-                      {project.description_italics}
-                    </i>
-                  </p>
-                  <div id="visit-btns">
-                    <a
-                      href={git_link + project.link}
-                      target="_blank"
-                      className="button code"
-                    >
-                      Code
-                    </a>
-                    <a
-                      href={site_link + project.link}
-                      target="_blank"
-                      className="button live"
-                    >
-                      Live
+        </section>
+        <div id="projects-ref" className="locator"></div>
+        <section id="projects">
+          <div className="section-body">
+            <h1 className="section-title">Work Samples</h1>
+            <div className="project-links">
+              {projects.map((project) => (
+                <div className="project-tile" id={project.id}>
+                  <div className="img-wrapper">
+                    <a href={site_link + project.link} target="_blank">
+                      <img
+                        id={project.img_id}
+                        src={project.img_src as string}
+                        alt={project.img_alt}
+                      />
                     </a>
                   </div>
+                  <div className="description">
+                    <p>
+                      <a
+                        href={site_link + project.link}
+                        target="_blank"
+                        className="project-link"
+                      >
+                        {project.title}
+                      </a>
+                      {project.wip ? (
+                        <div>
+                          <i className="wip">Work in Progress</i>
+                        </div>
+                      ) : (
+                        <br></br>
+                      )}
+                      {project.description}
+                      <i className="project-italics">
+                        {project.description_italics}
+                      </i>
+                    </p>
+                    <div id="visit-btns">
+                      <a
+                        href={git_link + project.link}
+                        target="_blank"
+                        className="button code"
+                      >
+                        Code
+                      </a>
+                      <a
+                        href={site_link + project.link}
+                        target="_blank"
+                        className="button live"
+                      >
+                        Live
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className="project-tile" id="codepen-project">
+                <div className="codepen-wrapper">
+                  <a href="https://codepen.io/rebecca-shoptaw" target="_blank">
+                    <img
+                      id="codepen"
+                      src="https://seeklogo.com/images/C/codepen-logo-FDEB3664F1-seeklogo.com.png"
+                    />
+                  </a>
+                  <a
+                    className="project-link"
+                    id="profile-link"
+                    href="https://codepen.io/rebecca-shoptaw"
+                    target="_blank"
+                  >
+                    Full Portfolio
+                  </a>
                 </div>
               </div>
-            ))}
-            <div className="project-tile" id="codepen-project">
-              <div className="codepen-wrapper">
+            </div>
+          </div>
+        </section>
+        <div id="contact-ref" className="locator"></div>
+        <section id="contact">
+          <div className="section-body">
+            <h1 className="section-title">Contact</h1>
+            <div id="contact-body">
+              <div className="letter-wrap">
+                <img
+                  id="letter"
+                  src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/352203/769892/restricted"
+                />
+              </div>
+              <div className="social-icons">
+                <a href="mailto:rebecca.shoptaw@gmail.com">
+                  <img
+                    src="https://icons.veryicon.com/png/o/internet--web/billion-square-cloud/mail-213.png"
+                    className="social-icon"
+                    id="mail"
+                  />
+                </a>
                 <a href="https://codepen.io/rebecca-shoptaw" target="_blank">
                   <img
-                    id="codepen"
                     src="https://seeklogo.com/images/C/codepen-logo-FDEB3664F1-seeklogo.com.png"
+                    className="social-icon"
+                    id="codepen2"
+                  />
+                </a>
+                <a href="https://github.com/rebecca-shoptaw" target="_blank">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+                    className="social-icon"
+                    id="github"
                   />
                 </a>
                 <a
-                  className="project-link"
-                  id="profile-link"
-                  href="https://codepen.io/rebecca-shoptaw"
+                  href="https://www.instagram.com/rebeccashoptawfilms/"
                   target="_blank"
                 >
-                  Full Portfolio
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
+                    className="social-icon"
+                    id="insta"
+                  />
                 </a>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <div id="contact-ref" className="locator"></div>
-      <section id="contact">
-        <div className="section-body">
-          <h1 className="section-title">Contact</h1>
-          <div id="contact-body">
-            <div className="letter-wrap">
-              <img
-                id="letter"
-                src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/352203/769892/restricted"
-              />
-            </div>
-            <div className="social-icons">
-              <a href="mailto:rebecca.shoptaw@gmail.com">
-                <img
-                  src="https://icons.veryicon.com/png/o/internet--web/billion-square-cloud/mail-213.png"
-                  className="social-icon"
-                  id="mail"
-                />
-              </a>
-              <a href="https://codepen.io/rebecca-shoptaw" target="_blank">
-                <img
-                  src="https://seeklogo.com/images/C/codepen-logo-FDEB3664F1-seeklogo.com.png"
-                  className="social-icon"
-                  id="codepen2"
-                />
-              </a>
-              <a href="https://github.com/rebecca-shoptaw" target="_blank">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
-                  className="social-icon"
-                  id="github"
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/rebeccashoptawfilms/"
-                target="_blank"
-              >
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
-                  className="social-icon"
-                  id="insta"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 };
