e83d52e447b92242de5a64fb9771e9aa6ee1b4db
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-02-14 14:35:12 -0500

    Update bio arrangement and styling.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import { Fade } from "react-awesome-reveal";
 import { useReset } from "../hooks/useReset";
 import { RESUME_PATH } from "../data/SiteLinks";
 import BioText from "./BioText";
+import TechStack from "./TechStack";
 
 const About = () => {
   const resetUrl = useReset();
@@ -12,8 +13,22 @@ const About = () => {
         <h1 className="section-title">About</h1>
         <div className="bio tile">
           <section id="bio-block">
-            <BioText />
+            <section className="bio-img-text">
+              <BioText />
+              <Fade>
+                <img
+                  id="bio-img"
+                  src="https://images.squarespace-cdn.com/content/v1/58c9909ce58c627a188f8a64/ad9e7ee7-964e-4151-96a6-e99d81353fd2/20201007.JPG?format=2500w"
+                  alt="Profile picture"
+                  loading="lazy"
+                />
+              </Fade>
+            </section>
             <Fade>
+              <div className="divider" />
+            </Fade>
+            <TechStack />
+            <Fade className="btns-locator">
               <div className="btns-wrap">
                 <a className="button-nofill" href={RESUME_PATH} target="_blank">
                   Resume
@@ -28,14 +43,6 @@ const About = () => {
               </div>
             </Fade>
           </section>
-          <Fade>
-            <img
-              id="bio-img"
-              src="https://images.squarespace-cdn.com/content/v1/58c9909ce58c627a188f8a64/ad9e7ee7-964e-4151-96a6-e99d81353fd2/20201007.JPG?format=2500w"
-              alt="Profile picture"
-              loading="lazy"
-            />
-          </Fade>
         </div>
       </div>
     </section>
