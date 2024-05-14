89d80272e35680c5ed80e1b38277cb9040fb49e2
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-05-14 15:08:44 -0400

    Remove separate bio text section and animations from about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,44 +1,39 @@
-import { Fade } from "react-awesome-reveal";
-import { useReset } from "../hooks/useReset";
-import { RESUME_PATH } from "../data/SiteLinks";
-import BioText from "./BioText";
-
 const About = () => {
-  const resetUrl = useReset();
-
   return (
     <section id="about" className="section-wrap">
       <div className="section-body">
         <h1 className="section-title">About</h1>
-        <div className="bio tile">
-          <section id="bio-block">
-            <section className="bio-img-text">
-              <BioText />
-              <Fade>
-                <img
-                  id="bio-img"
-                  src="https://images.squarespace-cdn.com/content/v1/58c9909ce58c627a188f8a64/ad9e7ee7-964e-4151-96a6-e99d81353fd2/20201007.JPG?format=2500w"
-                  alt="Profile picture"
-                  loading="lazy"
-                />
-              </Fade>
-            </section>
-            <Fade className="btns-locator">
-              <div className="btns-wrap">
-                <a className="button-nofill" href={RESUME_PATH} target="_blank">
-                  Resume
-                </a>
-                <a
-                  className="button-fill"
-                  href="#contact-ref"
-                  onClick={resetUrl}
-                >
-                  Get in Touch
-                </a>
-              </div>
-            </Fade>
-          </section>
-        </div>
+
+        <section className="bio-text">
+          <p>
+            Hello! My name is Rebecca and I'm an NYC-based software engineer
+            with a track record of realizing ambitious projects with rapid speed
+            🚀 and minute precision 🔎.
+          </p>
+
+          <p>
+            I am currently an Engineering Fellow and
+            Frontend/Internationalization Lead on the Internet Archive's{" "}
+            <a
+              className="inline-link"
+              href="https://github.com/internetarchive/openlibrary"
+              target="_blank"
+            >
+              Open Library
+            </a>{" "}
+            project. 👩‍💻📚
+            <br></br>
+            <br></br>
+            You can see my latest contributions{" "}
+            <a
+              className="inline-link"
+              href="https://github.com/internetarchive/openlibrary/issues?q=author%3Arebecca-shoptaw"
+              target="_blank"
+            >
+              here
+            </a>
+          </p>
+        </section>
       </div>
     </section>
   );
